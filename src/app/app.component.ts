24cd3b8701d669c8bdf9c0d8e1ee89438b902b9b
Alejandro Burgos <alejandroburgos97@yahoo.com>
2021-10-18 12:06:18 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto005';
+  mensaje=""
+  actualizar(t: any){
+    this.mensaje =t+" se actualiza cada 10 segundos"
+  }
 }
